87839073c1f6a9a1d5890cc7329b6a936f3cf1e6
ewnd9 <ewndnine@gmail.com>
2023-07-09 15:26:09 +0300

    add explorer context action

--- src/extension.ts
+++ src/extension.ts
@@ -1,4 +1,6 @@
 import * as vscode from 'vscode';
+import * as fs from 'fs/promises';
+import * as path from 'path';
 
 interface RefactorPair {
 	source: string;
@@ -9,12 +11,16 @@ interface Refactor {
 	pairs: RefactorPair[];
 }
 
+interface FileInput {
+	fsPath: string
+}
+
 const refactors: Refactor[] = [];
 
 export function activate(context: vscode.ExtensionContext) {
-	const disposable = vscode.commands.registerCommand('copy-paste.copyPaste', async () => {
+	const disposable = vscode.commands.registerCommand('copy-paste.copyPaste', async (file?: FileInput) => {
 		try {
-			await runCopyPasteCommand();
+			await runCopyPasteCommand(file);
 		} catch (err) {
 			console.error(err);
 			vscode.window.showErrorMessage((err as Error).message);
@@ -26,9 +32,46 @@ export function activate(context: vscode.ExtensionContext) {
 
 export function deactivate() { }
 
-async function runCopyPasteCommand() {
-	const refactor = refactors.length > 0 ? await askFromExistingRefactors() : await askForNewRefactor();
-	await applyRefactor(refactor);
+async function runCopyPasteCommand(file?: FileInput) {
+	if (file?.fsPath) {
+		const isDir = await fs.stat(file.fsPath).then(file => file.isDirectory()).catch(() => false);
+		if (!isDir) {
+			throw new Error(`${file.fsPath} is not a directory`);
+		}
+
+		const refactor = await askForRefactor();
+		await refactorDir(file.fsPath, path.dirname(file.fsPath), refactor);
+	} else {
+		const refactor = await askForRefactor();
+		const selection = await getSelection();
+		await applyRefactorToSelection(selection, refactor);
+	}
+}
+
+async function refactorDir(dirName: string, rootDir: string, refactor: Refactor) {
+	// `recursive: true` is available only on node@20
+	for (const dirent of await fs.readdir(dirName, { withFileTypes: true })) {
+		// `.dirent.path` is available only on node@20
+		if (dirent.isDirectory()) {
+			await refactorDir(`${dirName}/${dirent.name}`, rootDir, refactor);
+		} else {
+			const filePath = `${dirName}/${dirent.name}`;
+			const content = await fs.readFile(filePath, 'utf-8');
+
+			const filePathWithoutRootDir = filePath.replace(rootDir, '');
+			const newFilePath = `${rootDir}${refactorString(filePathWithoutRootDir, refactor)}`;
+			const newContent = refactorString(content, refactor);
+
+			console.log({ filePath, newFilePath, content, newContent });
+
+			await fs.mkdir(path.dirname(newFilePath), { recursive: true });
+			await fs.writeFile(newFilePath, newContent);
+		}
+	}
+}
+
+async function askForRefactor() {
+	return refactors.length > 0 ? await askFromExistingRefactors() : await askForNewRefactor();
 }
 
 async function askFromExistingRefactors() {
@@ -53,9 +96,9 @@ async function askFromExistingRefactors() {
 
 async function askForNewRefactor() {
 	const input = await vscode.window.showInputBox({
-		placeHolder: "Search query",
-		prompt: "Run replacements pairs",
-		value: 'ge'
+		prompt: 'Set replacements pairs',
+		placeHolder: "channels groups channel group",
+		value: ''
 	});
 
 	const refactor = parseInput(input || '');
@@ -64,23 +107,39 @@ async function askForNewRefactor() {
 	return refactor;
 }
 
-async function applyRefactor(refactor: Refactor) {
+async function getSelection() {
 	const editor = vscode.window.activeTextEditor;
 	if (!editor) {
 		throw new Error(`editor is empty`);
 	}
 
 	const selection = editor.selection;
-	if (selection && !selection.isEmpty) {
-		const selectionRange = new vscode.Range(selection.start.line, selection.start.character, selection.end.line, selection.end.character);
-
-		const highlighted = editor.document.getText(selectionRange);
-		const newText = highlighted.replace(new RegExp(refactor.pairs[0].source, 'g'), refactor.pairs[0].replacement);
-
-		await editor.edit(editBuilder => {
-			editBuilder.replace(selectionRange, newText);
-		});
+	if (!selection || selection.isEmpty) {
+		throw new Error(`selection is empty`);
 	}
+
+	const range = new vscode.Range(selection.start.line, selection.start.character, selection.end.line, selection.end.character);
+	const text = editor.document.getText(range);
+
+	return {
+		editor,
+		range,
+		text,
+	}
+}
+
+async function applyRefactorToSelection(selection: { editor: vscode.TextEditor, range: vscode.Range, text: string }, refactor: Refactor) {
+	const newText = refactorString(selection.text, refactor);
+	await selection.editor.edit(editBuilder => {
+		editBuilder.replace(selection.range, newText);
+	});
+}
+
+function refactorString(text: string, refactor: Refactor) {
+	return refactor.pairs.reduce((acc, pair) => acc.replace(
+		new RegExp(pair.source, 'g'),
+		pair.replacement
+	), text);
 }
 
 function parseInput(input: string) {
@@ -92,10 +151,17 @@ function parseInput(input: string) {
 	const refactor: Refactor = { pairs: [] }
 	for (let i = 0; i < chunks.length; i += 2) {
 		refactor.pairs.push({
+			source: upperFirst(chunks[i]),
+			replacement: upperFirst(chunks[i + 1]),
+		}, {
 			source: chunks[i],
 			replacement: chunks[i + 1],
 		});
 	}
 
 	return refactor;
+}
+
+function upperFirst(text: string) {
+	return text[0].toUpperCase() + text.slice(1);
 }
